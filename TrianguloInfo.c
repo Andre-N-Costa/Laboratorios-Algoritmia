9a079f3de023ab6eaef9ab3e00cac20ed61fdc35
André Costa <74073671+TrappyDie@users.noreply.github.com>
2021-03-03 10:44:46 +0000

    Update TrianguloInfo.c

--- TrianguloInfo.c
+++ TrianguloInfo.c
@@ -2,6 +2,29 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <math.h>
+
+int max(int x, int y, int z) {
+if (x > y) {
+    if (x > z) return x;
+    else return z;
+}
+else if (y > z) return y;
+else return z;
+}
+int somaquad(int x,int y,int z) {
+if (max(x,y,z) == x) return ((z*z)+(y*y));
+else if (max(x,y,z) == y) return ((x*x)+(z*z));
+else return ((x*x)+(y*y));
+}
+
+char* tipo(int a,int b,int c)
+{
+if ((max(a,b,c) * max(a,b,c)) == somaquad(a,b,c)) return ("RETANGULO");
+else if ((a == b) && (b == c)) return ("EQUILATERO");
+else if ((a != b) && (b != c) && (a != c)) return ("ESCALENO");
+else if (((a != b) && (a == c)) || ((a == b) && ( b != c)) || ((c == b) && (c != a))) return ("ISOSCELES");
+return 0;
+}
 int main() {
     int a,b,c;
     float area,p,perimetro;
@@ -9,12 +32,11 @@ int main() {
     perimetro = a + b + c;
     p = perimetro / 2;
     area = sqrt(p*(p-a)*(p-b)*(p-c));
-    if ((abs(b - c) < a) && (a < (b + c)) && (abs(a - c) < b) && (b < a + c) && (abs(a - b) < c) && (c < a + b)) {
-        if ((((a > b) && (a > c)) && ((a*a)==(b*b)+(c*c))) || (((b > a) && (b > c)) && ((b*b)==(a*a)+(c*c))) || (((c > b) && (c > a)) && ((c*c)==(b*b)+(a*a)))) (printf("%s %.0f %.2f\n","RETANGULO",perimetro, area));
-        else if ((a == b) && (b == c)) (printf("%s %.0f %.2f\n","EQUILATERO",perimetro, area));
-        else if ((a != b) && (b != c) && (a != c)) (printf("%s %.0f %.2f\n","ESCALENO",perimetro, area));
-        else if (((a != b) && (a == c)) || ((a == b) && ( b != c)) || ((c == b) && (c != a))) (printf("%s %.0f %.2f\n","ISOSCELES",perimetro, area));
-        }
+
+    if ((abs(b - c) < a) && (a < (b + c)) && (abs(a - c) < b) && (b < a + c) && (abs(a - b) < c) && (c < a + b))
+        (printf("%s %.0f %.2f\n",tipo(a,b,c),perimetro, area));
+
+
     else printf("INVALIDO\n");
     return 0;
 }
